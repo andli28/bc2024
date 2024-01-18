2d3a2e9d63efd056713859b6d474a97894ba98b7
Andrew Li <lawfairachyt28@gmail.com>
2024-01-18 13:56:53 -0500

    use all processors for run_matches_test if you wanted to use 100% of your brain but slow down your computer

--- run_matches_test.py
+++ run_matches_test.py
@@ -24,7 +24,7 @@ initialMaps = ['DefaultHuge', 'DefaultLarge', 'DefaultMedium', 'DefaultSmall']
 customMaps = ['pathfinder', 'bridge_aquifer', 'buh', 'crashtest1', 'Maze', 'one_on_one', 'pathfinder2', 'two_moats',
               'kirby', 'diagonal_grid']
 
-maps = initialMaps #+ sprint1Maps # + customMaps
+maps = initialMaps + sprint1Maps # + customMaps
 mapsSet = set(maps)
 
 matches = set(product(bots, maps))
@@ -55,15 +55,11 @@ def run_subprocess(teamA, teamB, map):
         print('You are running on an unsupported platform')
     return output
 
-def run_match(bot, map, toAddWins):
+def run_match(bot, map):
     print("Running {} vs {} on {}".format(currentBot, bot, map))
     try:
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            future_to_outputA = executor.submit(run_subprocess, currentBot, bot, map)
-            future_to_outputB = executor.submit(run_subprocess, bot, currentBot, map)
-
-            outputA = future_to_outputA.result()
-            outputB = future_to_outputB.result()
+        outputA = run_subprocess(currentBot, bot, map)
+        outputB = run_subprocess(bot, currentBot, map)
         
     except subprocess.CalledProcessError as exc:
         print("Status: FAIL", exc.returncode, exc.output)
@@ -81,31 +77,32 @@ def run_match(bot, map, toAddWins):
     
     if winAString in outputA:
         numWins += 1
-        toAddWins += 1
     else:
         if not loseAString in outputA:
             return 'Error'
     if winBString in outputB:
         numWins += 1
-        toAddWins += 1
     else:
         if not loseBString in outputB:
             return 'Error'
-    return numWinsMapping[numWins] + ' (' + ', '.join([gameLengthA, gameLengthB]) + ')', toAddWins
+    return numWinsMapping[numWins] + ' (' + ', '.join([gameLengthA, gameLengthB]) + ')', numWins
 
 
 if __name__ == "__main__":
     results = {}
     # Run matches
     print('Running {} maps'.format(len(matches)))
-    for bot, map in matches:
-        # Verify match is valid
-        if not bot in botsSet or not map in mapsSet:
-            errors.append('Unable to parse bot={}, map={}'.format(bot, map))
-        # run run_match.py
-        
-        results[(bot, map)], toAddWins = run_match(bot, map, 0)
-        totalWins += toAddWins
+    
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        future_to_game = {executor.submit(run_match, bot, map): (bot, map) for bot, map in matches}
+        for future in concurrent.futures.as_completed(future_to_game):
+            print('Finished number {} of {}'.format(len(results) + 1, len(matches)))
+            bot, map = future_to_game[future]
+            try:
+                results[(bot, map)], toAddWins = future.result()
+                totalWins += toAddWins
+            except Exception as exc:
+                print('%r generated an exception: %s' % (bot, exc))
 
     # Construct table
     table = [[results.get((bot, map), 'N/A') for bot in bots] for map in maps]
