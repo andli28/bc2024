020aa12521be33a1df9da5299f98e89613fd0c02
Andrew Li <lawfairachyt28@gmail.com>
2024-01-25 00:44:57 -0500

    only run vs v6_3 to save runtime

--- run_matches_fast.py
+++ run_matches_fast.py
@@ -16,7 +16,7 @@ emojiMap = {
 errors = []
 currentBot = 'mainbot'
 
-bots = ['v6_2_SentryFix', 'v6_3']
+bots = ['v6_3']
 botsSet = set(bots)
 
 # See https://github.com/battlecode/battlecode24/blob/master/client/src/constants.ts
