80253082e338c9f1aaa8f686fcddf1b18f6222eb
Andrew Li <lawfairachyt28@gmail.com>
2024-01-27 11:29:35 -0500

    sync buhfuhsuh with mainbot

--- src/buhfuhsuh_plus/Info.java
+++ src/buhfuhsuh_plus/Info.java
@@ -1,36 +1,45 @@
 package buhfuhsuh_plus;
 
 import battlecode.common.*;
+import mainbot.utils.*;
 
 public class Info {
     public static RobotController rc;
-    public static int round_num;
+	public static int round_num;
     public static Team team;
     public static Team enemyTeam;
     public static int MAP_HEIGHT;
     public static int MAP_WIDTH;
     public static int VISION_DIST;
-
+    
     public static MapLocation[] spawnLocs;
+    public static IterableLocSet spawnLocsSet;
     public static MapLocation closestSpawn;
     public static int numFlagsNearbyNotPickedUp;
     public static MapLocation closestFlag;
     public static int closestFlagDist;
+    public static FlagInfo closestFlagInfo;
 
-    public static RobotInfo[] friendly_robots;
+    public static RobotInfo[] friendly_robots;    
     public static MapLocation centerOfFriendliesLocation;
 
     public static RobotInfo[] enemy_robots;
 
-    public static void initialize(RobotController rc) throws GameActionException {
+
+    public static void initialize(RobotController rc) throws GameActionException{
         Info.rc = rc;
         // TODO: Add other initialization code here (transfer some from RobotPlayer)
 
         spawnLocs = rc.getAllySpawnLocations();
+        spawnLocsSet = new IterableLocSet(spawnLocs.length);
+        for (int i = spawnLocs.length - 1; i >= 0; i--) {
+            spawnLocsSet.add(spawnLocs[i]);
+        }
+        //don't have to update iterable if not iterating through it.
 
     }
 
-    public static void update() throws GameActionException {
+    public static void update() throws GameActionException{
         friendly_robots = rc.senseNearbyRobots(-1, rc.getTeam());
 
         // find closest spawnLoc:
@@ -60,6 +69,7 @@ public class Info {
         // find closest flagLoc:
         MapLocation closestFlag = null;
         int closestFlagDist = Integer.MAX_VALUE;
+        FlagInfo closestFlagInfo = null;
         for (int i = nearbyFlags.length - 1; i >= 0; i--) {
             if (!nearbyFlags[i].isPickedUp()) {
                 int distSqToSpawn = rc.getLocation()
@@ -67,11 +77,13 @@ public class Info {
                 if (distSqToSpawn < closestFlagDist) {
                     closestFlagDist = distSqToSpawn;
                     closestFlag = nearbyFlags[i].getLocation();
+                    closestFlagInfo = nearbyFlags[i];
                 }
             }
         }
         Info.closestFlag = closestFlag;
         Info.closestFlagDist = closestFlagDist;
+        Info.closestFlagInfo = closestFlagInfo;
 
     }
 
