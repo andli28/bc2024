e0160993459d93590d0460327ff162bfd39ae150
eggggo <geneliu2009@gmail.com>
2024-01-29 23:24:01 -0500

    stunlock

--- run_matches_fast.py
+++ run_matches_fast.py
@@ -14,9 +14,9 @@ emojiMap = {
     'Error': ':heavy_exclamation_mark:'
 }
 errors = []
-currentBot = 'mainbot'
+currentBot = 'stunlock'
 
-bots = ['v9_1_USQuals_REAL']
+bots = ['mainbot']
 botsSet = set(bots)
 
 # See https://github.com/battlecode/battlecode24/blob/master/client/src/constants.ts
